7c8e5176fe58ac6d0821c57d7494a7706ce14571
Çetin Mıdıkoğlu <91723908+CETYN@users.noreply.github.com>
2022-04-14 07:42:43 +0300

    Create çin zodyak hayvanları.py

---
+++ çin zodyak hayvanları.py
@@ -0,0 +1,45 @@
+yılar=["sıçan","öküz","kaplan","tavşan","ejderha","yılan","at","koyun","maymun","horoz","köpek","domuz"]
+yıla=int(input("yılınızı giriniz:"))
+yıl=yıla%12
+
+if(yıla<0):
+    print("NEGATİF SAYI GİRDİNİZ!!!")
+    
+elif(yıl==0):
+    print("{}: Maymun Yılı".format(yıla))
+    
+elif(yıl==1):
+    print("{}: Horoz Yılı".format(yıla))
+    
+elif(yıl==2):
+    print("{}: Köpek Yılı".format(yıla))
+
+elif(yıl==3):
+    print("{}: Domuz Yılı".format(yıla))
+    
+elif(yıl==4):
+    print("{}: Sıçan Yılı")
+    
+elif(yıl==5):
+    print("{}: Öküz Yılı".format(yıla))
+    
+elif(yıl==6):
+    print("{}: Kaplan Yılı".format(yıla))
+elif(yıl==7):
+    
+    print("{}: Tavşan Yılı".format(yıla))
+    
+elif(yıl==8):
+    print("{}:Ejderha Yılı".format(yıla))
+
+elif(yıl==9):
+    print("{}: Yılan Yılı".format(yıla))
+    
+elif(yıl==10):
+    print("{}: At yılı".format(yıla))   
+
+elif(yıl==11):
+    print("{}: Koyun yılı".format(yıla))    
+    
+
+    
